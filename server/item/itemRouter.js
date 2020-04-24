aba1c641aafd23054b4abc2ebf5221e7e7a38595
WarrenMfg <warrenmfg@icloud.com>
2020-04-24 14:55:45 -0400

    ellipsis added to overflow text for list items

--- server/item/itemRouter.js
+++ server/item/itemRouter.js
@@ -3,14 +3,14 @@ const controller = require('./itemController');
 
 
 Router.route('/one')
-  .get(controller.getOne)
+  .get(controller.getOne) // not used
   .post(controller.createOne)
   .put(controller.updateOne);
 
 Router.route('/one/:id')
   .delete(controller.deleteOne);
 
-Router.route('/many')
+Router.route('/many') // not used
   .get(controller.getMany);
 
 Router.route('/all')
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -15,11 +15,11 @@ class Item extends React.Component {
       const { _id, title, author, description, price } = this.props.item;
       return (
         <div className="Item Item-book" onClick={this.props.handleEditOrDelete} data-id={_id}>
-          <p className="Item-title">{title}</p>
-          <p className="Item-author">{author}</p>
-          <p className="Item-description">{description}</p>
-          <p className="Item-price">{price}</p>
-          <p className="Item-crud"><i className="fas fa-edit"></i><i className="fas fa-trash-alt"></i></p>
+          <p className="Item-title" title={title}>{title}</p>
+          <p className="Item-author" title={author}>{author}</p>
+          <p className="Item-description" title={description}>{description}</p>
+          <p className="Item-price" title={`$${price.toFixed(2)}`}>{`$${price.toFixed(2)}`}</p>
+          <p className="Item-crud"><i className="fas fa-edit" alt="Edit" title="Edit"></i><i className="fas fa-trash-alt" alt="Delete" title="Delete"></i></p>
         </div>
       );
 
@@ -27,11 +27,11 @@ class Item extends React.Component {
       const { _id, name, volume, issue, description, price } = this.props.item;
       return (
         <div className="Item Item-magazine" onClick={this.props.handleEditOrDelete} data-id={_id}>
-          <p className="Item-name">{name}</p>
-          <p className="Item-volume-span"><span>{volume}</span> <span>{issue}</span></p>
-          <p className="Item-description">{description}</p>
-          <p className="Item-price">{price}</p>
-          <p className="Item-crud"><i className="fas fa-edit"></i><i className="fas fa-trash-alt"></i></p>
+          <p className="Item-name" title={name}>{name}</p>
+          <p className="Item-volume-span" title={`v:${volume} i:${issue}`}><span>v:{volume}</span>&nbsp;<span>i:{issue}</span></p>
+          <p className="Item-description" title={description}>{description}</p>
+          <p className="Item-price" title={`$${price.toFixed(2)}`}>{`$${price.toFixed(2)}`}</p>
+          <p className="Item-crud"><i className="fas fa-edit" alt="Edit" title="Edit"></i><i className="fas fa-trash-alt" alt="Delete" title="Delete"></i></p>
         </div>
       );
     }
